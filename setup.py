f435bfd0d457cfecd83a080672e605c4f4ac143c
Abdullah Hashmi <abdullahhashmi01@gmail.com>
2026-06-24 07:20:05 +0500

    SetUp

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mental_health_chatbot",
+    version="0.1.0",
+    description="Fine-tuned empathetic chatbot (DistilGPT2 + EmpatheticDialogues)",
+    packages=find_packages(),
+    install_requires=[],
+)
